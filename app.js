ab61f228ff077125dd1db4dd4d6b695074afd263
Nikhil Srivastava <niksrc@gmail.com>
2016-02-17 22:10:52 +0530

    Add env for credentials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
