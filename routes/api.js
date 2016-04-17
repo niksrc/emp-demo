c3a3319b4ca5eca6cbf0953e4b36d9ea1ea4f0ce
Nikhil Srivastava <niksrc@gmail.com>
2016-04-17 14:05:33 +0530

    Adds categorization

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var emp = require('../lib/emp');
 var router = express.Router();
 
 /* Process emails. */
 router.post('/process', function(req, res, next) {
-  res.send({});
+  var result = emp.process(JSON.stringify(req.body));
+  res.send(result);
 });
 
 module.exports = router;
