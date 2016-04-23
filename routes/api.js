450108220d7c5a78eff9940b5930eda1b53399a6
Nikhil Srivastava <niksrc@gmail.com>
2016-04-23 22:54:11 +0530

    Refactored processing api to use email from ctxio

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var emp = require('../lib/emp');
 var router = express.Router();
+var ctx = require('../lib/ctxapi');
 
 /* Process emails. */
 router.post('/process', function(req, res, next) {
-  var result = emp.process(req.body.email);
-  res.send(result);
+  	var userid = req.body.userid;
+  	var msgid = req.body.msgid;
+
+		ctx.accounts(userid).messages(msgid).body().get(function (err, response) {
+			if(err)
+				throw err;
+			res.send(response);
+		});
 });
 
 module.exports = router;
